8d6016672f949304329587155bdf057f4c04bdd2
JackPan0521 <jack621252423@gmail.com>
2026-06-18 20:32:21 +0800

    新增背景音樂＆地圖樣式

--- GameProject/MyGame.java
+++ GameProject/MyGame.java
@@ -6,7 +6,8 @@ public class MyGame {
     public static void main(String[] args) {
 //Step 1. 產生遊戲背景物件: 標題, 寬度, 高度, 背景顏色
     GameContext ctx = new GameContext ("MY Warrior Game", 1080, 720, Color.white){
-        @Override
+        
+        /*@Override
         public String getBackgroundImgPath(){
             return "background.png";
         }
@@ -14,7 +15,7 @@ public class MyGame {
         @Override
         public String getBackgroundMusicPath(){
             return "Gameboy.mp3";
-        }
+        }*/
     } ;
 //Step 2. 產生遊戲物件 
     Game gameEngine = new Game(ctx); //Game就是遊戲引擎
@@ -22,10 +23,10 @@ public class MyGame {
     ArrayList<Role> myroles = new ArrayList<> (); //建立角色清單
     //[act][dir]: act:0 stop, act 1: walk, act 2: fly
     ImageSequence[][] is = {  { new ImageSequence("src/stop/" , "png", 1)},
-                              { new ImageSequence("src/walk_right/" , "png", 8),
-                                new ImageSequence("src/walk_left/" , "png", 8),
-                                new ImageSequence("src/walk_up/" , "png", 8),
-                                new ImageSequence("src/walk_down/" , "png", 8)}, 
+                              { new ImageSequence("src/walk_right/" , "png", 9),
+                                new ImageSequence("src/walk_left/" , "png", 9),
+                                new ImageSequence("src/walk_up/" , "png", 9),
+                                new ImageSequence("src/walk_down/" , "png", 9)}, 
                               { new ImageSequence("src/fly_right/" , "png", 1), 
                                 new ImageSequence("src/fly_left/" , "png", 1) },
                               { new ImageSequence("src/special_move/" , "png", 6) }
